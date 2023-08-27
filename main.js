1eb8d67ad1d649cb3d3c55a680408001be1a6eb8
FreezeMandu <72457223+FreezeMandu@users.noreply.github.com>
2023-08-27 19:04:47 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+UnitTypes.alpha.mineSpeed = 200
+UnitTypes.alpha.itemCapacity = 1000
+UnitTypes.alpha.health = 230
+Blocks.duo.reload = 0.5
+Blocks.arc.reload = 9
+Blocks.mechanicalDrill.drillTime = 100
+Blocks.conveyor.speed = 0.06
+Blocks.conveyor.health = 60
+Blocks.copperWall.health = 2000
+Blocks.coreShard.itemCapacity = 20000
