e88fef612a81f3dfc731846301afaea0a556add7
FreezeMandu <72457223+FreezeMandu@users.noreply.github.com>
2023-09-23 16:59:09 +0900

    Update no reloading_main.js

--- no reloading_main.js
+++ no reloading_main.js
@@ -2,6 +2,11 @@ UnitTypes.alpha.buildSpeed = 1.0
 UnitTypes.alpha.mineSpeed = 200
 UnitTypes.alpha.itemCapacity = 1000
 UnitTypes.alpha.health = 230
+UnitTypes.beta.buildSpeed = 1.25
+UnitTypes.beta.mineSpeed = 250
+UnitTypes.beta.itemCapacity = 1000
+UnitTypes.beta.health = 310
+
 
 Blocks.duo.reload = 0.5
 Blocks.arc.reload = 9.0
@@ -15,9 +20,7 @@ Blocks.overflowGate.health = 75
 Blocks.copperWall.health = 2000
 
 Blocks.coreShard.itemCapacity = 20000
-Blocks.coreShard.buildVisibility = true
-Blocks.coreShard.alwaysUnlocked = true
-Blocks.coreFoundation.unitType = UnitTypes.alpha
+Blocks.coreShard.buildVisibility = shown
 
 Blocks.solarPanel.powerProduction = 2
 Blocks.steamGenerator.powerProduction = 8
