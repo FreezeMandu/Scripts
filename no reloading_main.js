1ef60dacc6ac579959372fc0a9b6e0e08ac2aea9
FreezeMandu <72457223+FreezeMandu@users.noreply.github.com>
2023-09-24 10:30:26 +0900

    Update no reloading_main.js

--- no reloading_main.js
+++ no reloading_main.js
@@ -2,7 +2,7 @@ UnitTypes.alpha.buildSpeed = 1.0
 UnitTypes.alpha.mineSpeed = 200
 UnitTypes.alpha.itemCapacity = 1000
 UnitTypes.alpha.health = 230
-UnitTypes.beta.buildSpeed = 1.25
+UnitTypes.beta.buildSpeed = 1.35
 UnitTypes.beta.mineSpeed = 250
 UnitTypes.beta.itemCapacity = 1000
 UnitTypes.beta.health = 310
@@ -11,7 +11,7 @@ UnitTypes.beta.health = 310
 Blocks.duo.reload = 0.5
 Blocks.arc.reload = 9.0
 
-Blocks.mechanicalDrill.drillTime = 140
+Blocks.mechanicalDrill.drillTime = 100
 Blocks.mechanicalDrill.tier = 2
 Blocks.pneumaticDrill.drillTime = 100
 Blocks.pneumaticDrill.tier = 4
@@ -26,6 +26,7 @@ Blocks.coreShard.itemCapacity = 20000
 
 Blocks.solarPanel.powerProduction = 2
 Blocks.steamGenerator.powerProduction = 8
+Blocks.powerNode.health = 75
 
 
 
