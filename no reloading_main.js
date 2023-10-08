793a77a1788c3b359289f5740e3e7195eb8fc089
FreezeMandu <72457223+FreezeMandu@users.noreply.github.com>
2023-10-08 16:42:02 +0900

    Update no reloading_main.js

--- no reloading_main.js
+++ no reloading_main.js
@@ -26,7 +26,6 @@ Blocks.router.health = 75
 Blocks.overflowGate.health = 75
 
 Blocks.copperWall.health = 2000
-Blocks.copperWall.chanceDeflect = 20
 Blocks.copperWallLarge.health = 5500
 Blocks.copperWallLarge.absorbLasers = true
 Blocks.copperWallLarge.lightningChance = 20
