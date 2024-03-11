4a410aa6f3db5e273bc48f8b5a321efaab4ae0a6
FreezeMandu <72457223+FreezeMandu@users.noreply.github.com>
2024-03-11 09:27:27 +0900

    Update targetable.js

--- targetable.js
+++ targetable.js
@@ -2,6 +2,7 @@
 Events.on(EventType.ClientLoadEvent, e=>{
     Vars.content.bullets().each(b => b instanceof BasicBulletType, b => b.frontRegion = Core.atlas.find("router"));
     });
-*/
+주석은 삭제하고 복사할것*/
 
 Vars.content.blocks().each(a => a instanceof BaseTurret, a => a.targetable = false)
+Vars.content.blocks().each(a => a instanceof Wall, a => a.targetable = false)
