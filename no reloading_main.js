0e654422df17ec08919f288937fdddb8f90f8754
FreezeMandu <72457223+FreezeMandu@users.noreply.github.com>
2025-02-04 18:19:51 +0900

    Add files via upload

---
+++ no reloading_main.js
@@ -0,0 +1,26 @@
+UnitTypes.alpha.buildSpeed = 1.2
+UnitTypes.alpha.mineSpeed = 200
+UnitTypes.alpha.itemCapacity = 1000
+UnitTypes.alpha.health = 230
+
+UnitTypes.beta.buildSpeed = 1.4
+UnitTypes.beta.mineSpeed = 300
+UnitTypes.beta.itemCapacity = 1000
+UnitTypes.beta.health = 250
+
+UnitTypes.gamma.buildSpeed = 1.6
+UnitTypes.gamma.mineSpeed = 400
+UnitTypes.gamma.itemCapacity = 1000
+UnitTypes.gamma.health = 270
+
+Blocks.duo.reload = 0.5
+Blocks.arc.reload = 9.0
+Blocks.mechanicalDrill.drillTime = 100
+Blocks.conveyor.speed = 0.06
+Blocks.conveyor.health = 60
+Blocks.copperWall.health = 2000
+Blocks.coreShard.itemCapacity = 20000
+Blocks.coreFoundation.itemCapacity = 40000
+Blocks.coreNucleus.itemCapacuty = 60000
+Blocks.coreShard.health = 6000
+Blocks.coreFoundation.health = 6000
